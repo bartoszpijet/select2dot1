2753e38f8c6b422e7804513587c484b90a81de33
patrykborawski-inverno <137785849+patrykborawski-inverno@users.noreply.github.com>
2023-08-24 13:23:53 +0200

    Update search_controller.dart
    Signed-off-by: patrykborawski-inverno <137785849+patrykborawski-inverno@users.noreply.github.com>

--- lib/src/controllers/search_controller.dart
+++ lib/src/controllers/search_controller.dart
@@ -69,9 +69,9 @@ class SearchControllerSelect2dot1 extends ChangeNotifier {
               threshold: 0.5,
             ),
       );
-      List<Result<SingleItemCategoryModel>> results = await fuse.search(value);
-
-      for (var element in results) {
+      List<Result<SingleItemCategoryModel>> tmpResults =
+          await fuse.search(value);
+      for (var element in tmpResults) {
         if (element.identifier != null) {
           // Null check done above.
           // ignore: avoid-non-null-assertion
